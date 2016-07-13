15f59fe7d8ab8e9294df25962a2707828ee04b92
Mike Barkas <mike@barkas.com>
2016-07-12 20:00:53 -0400

    Add docstring to class.

--- bingo_cage.py
+++ bingo_cage.py
@@ -2,19 +2,16 @@ import random
 
 
 class BingoCage:
-
-
+    """A callable class that will return a pick from the cage."""
     def __init__(self, items):
         self._items = list(items)
         random.shuffle(self._items)
-
 
     def pick(self):
         try:
             return self._items.pop()
         except IndexError:
             raise LookupError('The bingo cage is empty')
-
 
     def __call__(self):
         return self.pick()
